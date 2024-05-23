0cf4fefd6955d6a36586cc2e190dd7d6363ebc42
MiracleMaker32 <stu2301561029@uni-plovdiv.bg>
2024-05-23 23:11:23 +0300

    Add files via upload

---
+++ ArenaGameEngine/DragonSlayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGameEngine
+{
+    public class DragonSlayer : Hero
+    {
+        public DragonSlayer() : this("Garuda") //set cool name("Garuda")
+        {
+
+        }
+
+        public DragonSlayer(string name) : base(name)
+        {
+        
+        }
+
+        public override int Attack()
+        {
+            int baseAttack = base.Attack();
+            // Let's say DragonSlayer has a 50% chance to deal quadruple damage(I want him to be the strongest heh)
+            if (Random.Shared.Next(0, 100) < 50)
+            {
+                Console.WriteLine($"{Name} strikes with unmatched power!");
+                baseAttack *= 4;
+            }
+            return baseAttack;
+        }
+
+        public override void TakeDamage(int incomingDamage)
+        {
+            // Let's say DragonSlayer has 50% armor which reduces incoming damage
+            int reducedDamage = incomingDamage - (int)(incomingDamage * 0.5);
+            base.TakeDamage(reducedDamage);
+        }
+    }
+}
---
+++ ArenaGameEngine/Priest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGameEngine
+{
+    public class Priest : Hero
+    {
+        public Priest() : this("Rudolph") // Cool name 'Rudolph'
+        {
+
+        }
+
+        public Priest(string name) : base(name) 
+        {
+        }
+
+        public override int Attack()
+        {
+            int baseAttack = base.Attack();
+            // Implement attack logic with a chance to summon minions
+            if (Random.Shared.Next(0, 100) < 40)
+            {
+                Console.WriteLine($"{Name} summons a minion to aid in the attack!");
+                baseAttack += 20; // Increase attack by 20
+            }
+            return baseAttack;
+        }
+
+        public override void TakeDamage(int incomingDamage)
+        {
+            // Implement damage-taking logic with a chance to deal thorns damage
+            // Example: Deal thorns damage with a probability of 50%
+            if (Random.Shared.Next(0, 100) < 50)
+            {
+                int thornsDamage = incomingDamage / 2; // Deal half of the incoming damage as thorns damage
+                Console.WriteLine($"{Name} retaliates with thorns damage!");
+                base.TakeDamage(thornsDamage); // Apply thorns damage to the attacker
+            }
+            else
+            {
+                base.TakeDamage(incomingDamage); 
+            }
+        }
+    }
+}
